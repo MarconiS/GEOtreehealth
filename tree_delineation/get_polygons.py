fixed the issue with hsi channels. not sure I like the normalization

tree_delineation/get_polygons.py
@@ -143,7 +143,7 @@ def predict_tree_crowns(batch, input_points, neighbors = 3,
         input_boxes = input_boxes.astype(int)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    sam = sam_model_registry[config.model_type](checkpoint=config.sam_checkpoint)
 
     #neighbors must be the minimum between the total number of input_points and the argument neighbors
     #neighbors = min(input_points.shape[0]-2, neighbors)
@@ -675,7 +675,12 @@ def mask_to_delineation(mask, center, rgb = True, buffer_size = 0):
     min_x, min_y = np.min(non_zero_indices, axis=1)
     max_x, max_y = np.max(non_zero_indices, axis=1)
     # Define the center of the smallest region
+    if(max_x == min_x):
+        max_x = max_x + 1
 
+    if(max_y == min_y):
+        max_y = max_y + 1
+        
     if buffer_size is not None:
         #identify buffer around the target to dramatically increase efficency
         submask = labeled_mask[int(min_x):int(max_x), int(min_y):int(max_y)]

tree_health_detection/build_dataloaders.py
@@ -58,7 +58,10 @@ class MultiModalDataset(Dataset):
         hsi = self.preprocess(hsi) #, mask = None) #, np.load(mask_hsi_path))
         rgb = self.preprocess_rgb(rgb)#, mask = None) #, np.load(mask_rgb_path))
         lidar = self.normalize_point_cloud(lidar)
-        
+
+        # rearrange hsi from x,y, z to z,y,x
+        hsi = np.transpose(hsi, (2, 0, 1))
+
         sample = {'hsi': hsi, 'rgb': rgb, 'lidar': lidar,  'label': label}
 
         return sample

tree_health_detection/main.py
@@ -18,6 +18,8 @@ import pickle
 import rasterio
 import torch
 import matplotlib.pyplot as plt
+import importlib
+
 
 import tree_delineation
 import tree_health_detection
@@ -27,7 +29,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import tempfile
-
+from pathlib import Path
+import shutil
 #import config.py
 import config
 
@@ -65,7 +68,7 @@ def __main__():
         
     #get the crowns that match with stem_path
     if config.crowns is not None:
-        crowns = gpd.read_file(config.data_path + config.crowns + '/SAM_'+config.siteID+'.gpkg', driver='GPKG')
+        crowns = gpd.read_file(config.data_path + config.crowns + '/SAM_'+config.siteID+'_manual.gpkg', driver='GPKG')
     else:
         #loop through geopackages in custom folder and append polygons in a unique geodataframe
         if config.get_tree_crowns:
@@ -95,20 +98,26 @@ def __main__():
 
     # if data storage required
     if config.store_clips:
-        rgb_dir = config.root_dir / "rgb"
-        hsi_dir = config.root_dir / "hsi"
-        png_dir = config.root_dir / "png"
+        if config.clean_dataset:
+            # remove directories and their content if they exist
+            for dir in [ config.root_dir + "rgb", config.root_dir + "hsi", config.root_dir + "lidar", config.root_dir + "polygon_mask", config.root_dir + "labels"]:
+                if os.path.exists(dir):
+                    shutil.rmtree(dir)
 
-        lidar_dir = config.root_dir / "lidar"
-        polygon_mask_dir = config.root_dir / "polygon_mask"
-        labels_dir = config.root_dir / "labels"
+        rgb_dir = config.root_dir + "rgb"
+        hsi_dir = config.root_dir + "hsi"
+        png_dir = config.root_dir + "png"
+        lidar_dir = config.root_dir + "lidar"
+        polygon_mask_dir = config.root_dir + "polygon_mask"
+        labels_dir = config.root_dir + "labels"
 
-        # Ensure that all directories exist
-        for dir in [config.root_dir, rgb_dir, hsi_dir, lidar_dir, polygon_mask_dir, labels_dir]:
-            dir.mkdir(parents=True, exist_ok=True)
+        # Ensure that all directories exist. if directories do not exist, create them
+        for dir in [config.root_dir, rgb_dir, hsi_dir, png_dir, lidar_dir, polygon_mask_dir, labels_dir]:
+            if not os.path.exists(dir):
+                os.makedirs(dir)
 
         # Load your polygon data (GeoDataFrame)
-        gdf = gpd.read_file(config.data_path+crowns)
+        gdf = gpd.read_file(config.data_path + config.crowns + '/SAM_'+config.siteID+'_manual.gpkg', driver='GPKG')
         itcs = gpd.read_file( config.data_path+config.stem_path)
 
         # select only the columns needed and turn into dataframe
@@ -117,8 +126,9 @@ def __main__():
 
         # Process each polygon
         for idx, row in gdf.iterrows():
-            tree_health_detection.store_data_structures.process_polygon(polygon = row, root_dir = config.root_dir,  rgb_path = config.data_path+config.rgb_path, 
-                            hsi_path = config.data_path+hsi_img, 
+            tree_health_detection.store_data_structures.process_polygon(polygon = row,
+                            root_dir = config.root_dir,  rgb_path = config.data_path+config.rgb_path, 
+                            hsi_path = config.data_path+config.hsi_img, 
                             lidar_path = config.data_path+config.laz_path, polygon_id=  idx, itcs=itcs)
 
     ### move to main script
@@ -254,7 +264,9 @@ def __main__():
 
     # Transform function to resize the image
     resize = transforms.Resize((config.hsi_resize, config.hsi_resize))
-
+    del hsi, rgb, lidar, labels
+    del  hsi_, rgb_, lidar_, labels_
+    torch.cuda.empty_cache()
     # Test Set Evaluation
     model.eval()
     test_preds = []
@@ -316,5 +328,3 @@ def __main__():
 
     return(model)
 
-if __name__ == '__main__':
-    __main__()

tree_health_detection/store_data_structures.py
@@ -70,19 +70,22 @@ def extract_data_cube_lidar(dataset_path, polygon, output_path):
 def cumulative_linear_stretch(image, background_mask=None):
     if background_mask is not None:
         # Apply background mask
-        background_mask = background_mask.astype(np.bool)        
+        background_mask = background_mask.astype(bool)        
 
     # Apply cumulative linear stretch to each band
-    stretched_image = np.zeros_like(image, dtype=np.float)
+    stretched_image = np.zeros_like(image, dtype=np.float64)
     for band in range(3):
         # Select only the pixels where the mask is False for each band
-        band_image = image[:, :, band][background_mask == True]
+        if background_mask is not None:
+            band_image = image[:, :, band][background_mask == True]
+        else:
+            band_image = image[:, :, band]
+
         # Calculate minimum and maximum values
         min_value = np.min(band_image)
         max_value = np.max(band_image)
         # Apply linear stretch
         stretched_image[:, :, band] = (image[:, :, band] - min_value) / (max_value - min_value) * 255
-
 
     stretched_image[stretched_image <0] = 0
     # Convert to 8-bit unsigned integers (uint8)
@@ -117,37 +120,38 @@ def png_with_class(dataset_path, polygon, bool_mask, output_path, status_label):
         rgb_image[rgb_image < 0] = 0
         rgb_image[rgb_image > 10000] = 10000
 
-        # Normalize the image in range 0, 255
-        rgb_image = (rgb_image - rgb_image.min()) / (rgb_image.max() - rgb_image.min()) * 255
-        rgb_image = cumulative_linear_stretch(rgb_image, binary_mask)
+        if rgb_image.max() > 0:
+            # Normalize the image in range 0, 255
+            rgb_image = (rgb_image - rgb_image.min()) / (rgb_image.max() - rgb_image.min()) * 255
+            rgb_image = cumulative_linear_stretch(rgb_image, binary_mask)
 
-        # Create PIL image from numpy array
-        rgb_image = rgb_image.astype(np.uint8)
-        pil_image = Image.fromarray(rgb_image)
+            # Create PIL image from numpy array
+            rgb_image = rgb_image.astype(np.uint8)
+            pil_image = Image.fromarray(rgb_image)
 
-        new_width = rgb_image.shape[1] *10
-        new_height = rgb_image.shape[0] *10
-        pil_image = pil_image.resize((new_width, new_height))
-        # Add text label on the image
-        #draw = ImageDraw.Draw(pil_image)
-        #text = status_label
-        #text_color = (255, 255, 255)  # White color
-        #text_position = (20, 20)  # Position of the text label
-        #draw.text(text_position, text, fill=text_color)
-        # Save the modified image as PNG
-        pil_image.save(output_path, format='PNG')
+            new_width = rgb_image.shape[1] *10
+            new_height = rgb_image.shape[0] *10
+            pil_image = pil_image.resize((new_width, new_height))
+            # Add text label on the image
+            #draw = ImageDraw.Draw(pil_image)
+            #text = status_label
+            #text_color = (255, 255, 255)  # White color
+            #text_position = (20, 20)  # Position of the text label
+            #draw.text(text_position, text, fill=text_color)
+            # Save the modified image as PNG
+            pil_image.save(output_path, format='PNG')
 
 
 # Function to process each polygon
 def process_polygon(polygon, root_dir, rgb_path, hsi_path, lidar_path, polygon_id, itcs):
 
-    rgb_dir = root_dir / "rgb"
-    hsi_dir = root_dir / "hsi"
-    png_dir = root_dir / "png"
+    rgb_dir = Path(root_dir) / "rgb"
+    hsi_dir = Path(root_dir) / "hsi"
+    png_dir = Path(root_dir) / "png"
 
-    lidar_dir = root_dir / "lidar"
-    polygon_mask_dir = root_dir / "polygon_mask"
-    labels_dir = root_dir / "labels"
+    lidar_dir = Path(root_dir) / "lidar"
+    polygon_mask_dir = Path(root_dir) / "polygon_mask"
+    labels_dir = Path(root_dir) / "labels"
 
     # Extract and save data cubes
     extract_data_cube(dataset_path = rgb_path, polygon = polygon.geometry, output_path = rgb_dir / f"{polygon_id}.npy")
@@ -160,12 +164,12 @@ def process_polygon(polygon, root_dir, rgb_path, hsi_path, lidar_path, polygon_i
     label = itcs.loc[itcs['StemTag'] == polygon.StemTag] 
     # add polygon_id to the label and the path of the rgb, hsi and lidar extracted cubes
     label['polygon_id'] = polygon_id
-    label['rgb_path'] = rgb_dir / f"{polygon_id}.npy"
-    label['hsi_path'] = hsi_dir / f"{polygon_id}.npy"
-    label['lidar_path'] = lidar_dir / f"{polygon_id}.npy"
+    label['rgb_path'] = Path(rgb_dir) / f"{polygon_id}.npy"
+    label['hsi_path'] = Path(hsi_dir) / f"{polygon_id}.npy"
+    label['lidar_path'] = Path(lidar_dir) / f"{polygon_id}.npy"
     png_with_class(dataset_path = hsi_path, polygon = polygon.geometry, bool_mask = bool_mask,
                    output_path = png_dir / f"{label.Status.values[0]}_{polygon_id}.png", 
                    status_label = label.Status.values[0])
     # save the label
-    #label.to_csv(labels_dir / f"{polygon_id}.csv", index=False)
+    label.to_csv(labels_dir / f"{polygon_id}.csv", index=False)
     